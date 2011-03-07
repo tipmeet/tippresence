457478119f8c83d952b72da5de2dbd43e201f838
John Khvatov <ivaxer@imarto.net>
2011-03-07 20:35:38 +0300

    utils: implemented status_keyf() (key function for built-in min/max)

--- tippresence/utils.py
+++ tippresence/utils.py
@@ -8,3 +8,7 @@ from twisted.internet import reactor
 def random_str(len):
     return "".join(choice(ascii_letters) for x in xrange(len))
 
+def status_keyf(status):
+    priority = status['priority']
+    presence_status = 1 if status['presence']['status'] == 'online' else 0
+    return 2 * priority + presence_status
