122801f16ae26b25e6a272fa0df74b3fa8f89647
John Khvatov <ivaxer@imarto.net>
2011-03-14 02:36:43 +0300

    http: presence: updated for new API - added response() function

--- tippresence/http/presence.py
+++ tippresence/http/presence.py
@@ -5,13 +5,15 @@ import json
 from twisted.internet import defer
 from twisted.web import resource, server, http
 
-from tippresence import stats
-from tippresence import aggregate_status
-from tippresence import PresenceServiceError
+from tippresence import PresenceError
 
 from twisted.python import log
 
 success_reply = {'status': 'ok', 'reason': 'Success'}
+
+def response(status='failure', reason='Failed', result=None):
+    assert status in ['failure', 'ok']
+    return json.dumps({'status': status, 'reason': reason, 'result': result})
 
 class HTTPPresence(resource.Resource):
     isLeaf = True
@@ -23,44 +25,40 @@ class HTTPPresence(resource.Resource):
         return [x for x in path if x]
 
     def render_GET(self, request):
-        stats['http_received_requests'] += 1
         path = self._filterPath(request.postpath)
         if len(path) == 1:
-            return self.getStatus(request.write, request.finish, path[0])
+            return self.getPresence(request, path[0])
         elif len(path) == 0:
             if not self.authenticate(request):
-                return json.dumps({'status': 'failure', 'reason': 'auth required'})
-            return self.dumpStatuses(request.write, request.finish)
-        return json.dumps({'reason': 'Invalid URI', 'status': 'failure'})
+                return response("failure", "Authentication required")
+            return self.dumpAllPresence(request)
+        return response("failure", "Invalid URI")
 
     def render_PUT(self, request):
-        stats['http_received_requests'] += 1
         if not self.authenticate(request):
-            return json.dumps({'status': 'failure', 'reason': 'auth required'})
+            return response("failure", "Authentication required")
         path = self._filterPath(request.postpath)
         if len(path) == 1:
-            return self.putStatus(request.write, request.finish, path[0], request.content)
+            return self.putPresence(request, path[0], request.content)
         if len(path) == 2:
-            return self.putStatus(request.write, request.finish, path[0], request.content, tag=path[1])
-        return json.dumps({'reason': 'Invalid URI', 'status': 'failure'})
+            return self.putPresence(request, path[0], request.content, tag=path[1])
+        return response("failure", "Invalid URI")
 
     def render_DELETE(self, request):
-        stats['http_received_requests'] += 1
         if not self.authenticate(request):
-            return json.dumps({'status': 'failure', 'reason': 'auth required'})
+            return response("failure", "Authentication required")
         path = self._filterPath(request.postpath)
         if len(path) == 2:
-            return self.removeStatus(request.write, request.finish, path[0], path[1])
-        return json.dumps({'status': 'failure', 'reason': 'Invalid URI'})
+            return self.removePresence(request, path[0], path[1])
+        return response("failure", "Invalid URI")
 
     def render_POST(self, request):
-        stats['http_received_requests'] += 1
         if not self.authenticate(request):
-            return json.dumps({'status': 'failure', 'reason': 'auth required'})
+            return response("failure", "Authentication required")
         path = self._filterPath(request.postpath)
         if path:
-            return json.dumps({'status': 'failure', 'reason': 'Invalid URI'})
-        return self.putAllStatuses(request.write, request.finish, request.content)
+            return response("failure", "Invalid URI")
+        return self.putAllStatuses(request, request.content)
 
     def authenticate(self, request):
         if not self.users:
@@ -79,99 +77,86 @@ class HTTPPresence(resource.Resource):
             return
         return 1
 
-    def getStatus(self, write, finish, resource):
-        def reply(status):
-            res = {'status': 'ok', 'reason': 'success'}
-            res['result'] = status
-            write(json.dumps(res))
-            finish()
-
-        d = self.presence.getStatus(resource)
-        d.addCallback(aggregate_status)
+    def getPresence(self, request, resource):
+        def reply(presence):
+            if presence:
+                status = presence["status"]
+            else:
+                status = "offline"
+            request.setHeader("X-Presence-Status", status)
+            request.write(response("ok", "Success", status))
+            request.finish()
+        d = self.presence.get(resource)
         d.addCallback(reply)
         return server.NOT_DONE_YET
 
-    def dumpStatuses(self, write, finish):
-        def reply(r):
-            result = {}
-            for res, status in r.items():
-                result[res] = {}
-                result[res] = aggregate_status(status)
-            write(json.dumps({'status': 'ok', 'reason': 'Successfully dumped', 'result': result}))
-            finish()
+    def dumpAllPresence(self, request):
+        raise NotImplementedError
 
-        d = self.presence.dumpStatuses()
-        d.addCallback(reply)
-        return server.NOT_DONE_YET
-
-    def putStatus(self, write, finish, resource, content, tag=None):
+    def putPresence(self, request, resource, content, tag=None):
         def reply(tag):
-            result = {'tag': tag}
-            r = json.dumps({'reason': 'Status added', 'status': 'ok', 'result': result})
-            write(r)
-            finish()
-
-        def reply_error(failure):
-            failure.trap(PresenceServiceError)
-            msg = failure.getErrorMessage()
-            r = json.dumps({'status': 'failure', 'reason': msg})
-            write(r)
-            finish()
+            request.write(response("ok", "Success", {'tag': tag}))
+            request.finish()
 
         try:
             r = json.load(content)
         except ValueError, e:
-            return json.dumps({'reason': str(e), 'status': 'failure'})
-        args = []
+            return response("failure", "Invalid data: " + str(e))
+        if 'status' not in r:
+            return response("failure", "Presence status required")
+        status = r['status']
         kw = {}
-        args.append(resource)
-        args.append(r['presence'])
-        if not 'expires' in r:
-            return json.dumps({'reason': 'Expires required', 'status': 'failure'})
-        args.append(int(r['expires']))
-        if 'priority' in r:
-            kw['priority'] = int(r['priority'])
         if tag:
             kw['tag'] = tag
-        d = self.presence.putStatus(*args, **kw)
+        if 'priority' in r:
+            kw['priority'] = int(r['priority'])
+        if 'expires' in r:
+            kw['expires'] = int(r['expires'])
+        d = self.presence.put(resource, status, **kw)
         d.addCallback(reply)
-        d.addErrback(reply_error)
+        d.addErrback(self._replyError, request)
         return server.NOT_DONE_YET
 
-    def removeStatus(self, write, finish, resource, tag):
+    def removePresence(self, request, resource, tag):
         def reply(r):
-            r = json.dumps({'reason': r, 'status': 'ok'})
-            write(r)
-            finish()
+            if r:
+                request.write(response("ok", "Success"))
+            else:
+                request.write(response("failure", "Not Found"))
+            request.finish()
 
-        d = self.presence.removeStatus(resource, tag)
+        d = self.presence.remove(resource, tag)
         d.addCallback(reply)
         return server.NOT_DONE_YET
 
-    def putAllStatuses(self, write, finish, content):
+    def putAllStatuses(self, request, content):
         def reply(r):
-            write(json.dumps({'reason': 'Success', 'status': 'ok'}))
-            finish()
-        def reply_error(r):
-            write(json.dumps({'reason': 'Failed: %s' % str(r), 'status': 'failure'}))
-            finish()
+            request.write(response("ok", "Success"))
+            request.finish()
 
         try:
             docs = json.load(content)
         except ValueError, e:
-            return json.dumps({'reason': str(e), 'status': 'failure'})
-        d = []
+            return response("failure", str(e))
+        dl = []
         for (resource, r) in docs.items():
-            args = []
             kw = {}
-            args.append(resource)
-            args.append(r['presence'])
-            args.append(int(r['expires']))
+            #XXX: rework it
+            status = r['presence']['status']
+            expires = int(r['expires'])
             if 'priority' in r:
                 kw['priority'] = int(r['priority'])
             if 'tag' in r:
                 kw['tag'] = r['tag']
-            d.append(self.presence.putStatus(*args, **kw))
-        defer.DeferredList(d, fireOnOneErrback=True).addCallbacks(reply, reply_error)
+            dl.append(self.presence.put(resource, status, expires, **kw))
+        d = defer.DeferredList(dl, fireOnOneErrback=True)
+        d.addCallback(reply)
+        d.addErrback(self._replyError, request)
         return server.NOT_DONE_YET
+
+    def _replyError(self, failure, request):
+        failure.trap(PresenceError)
+        msg = failure.getErrorMessage()
+        request.write(response("failure", msg))
+        request.finish()
 
